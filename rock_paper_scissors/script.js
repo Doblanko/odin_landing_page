610358f6cb7b3a4a6acc5097893bbc59f4a89e39
Tyler  Doblanko <tylerdoblanko@gmail.com>
2021-12-21 19:57:43 -0700

    added UI to rock paper scissors

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -3,61 +3,64 @@ function computerPlay() {
     let hand = Math.floor(Math.random() * 3);
 
     switch(hand) {
-        case 0: 
-            return 'rock';
-            break;
-        case 1: 
-            return 'paper';
-            break;
-        case 2: 
-            return 'scissors';
-            break;
+        case 0: return 'rock';
+        case 1: return 'paper';
+        case 2: return 'scissors';
     };
 }
 
-function game() {
-    let score = [0, 0];
-    let computerSelection = '';
-    let playerSelection = '';
-    let roundResult;
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
 
-    for (let i = 1; i <= 5; i++) {
-        computerSelection = computerPlay();
-        playerSelection = prompt('Select rock, paper, or scissors:')
-        roundResult = playRound(playerSelection, computerSelection)
-
-        switch(roundResult[1]) {
-            case -1:
-                break;
-            case 0:
-                score[1]++
-                break;
-            case 1:
-                score[0]++
-                break;
-        }
-        console.log(roundResult[0] + ` The score is ${score[0]}-${score[1]}.`);
-    }
-
-    function playRound(playerSelection, computerSelection) {
-        playerSelection = playerSelection.toLowerCase();
+    let dict = {
+        'rockrock': ['Tie!', -1],
+        'paperpaper': ['Tie!', -1],
+        'scissorsscissors': ['Tie!', -1],
+        'rockpaper': ['You Lose! Paper beats rock.', 0],
+        'rockscissors': ['You Win! Rock beats scissors.', 1],
+        'paperrock': ['You Win! Paper beats rock.', 1],
+        'paperscissors': ['You Lose! Scissors beats paper.', 0],
+        'scissorsrock': ['You Lose! Rock beats scissors.', 0],
+        'scissorspaper': ['You Win! Scissors beats paper.', 1],
+    };
     
-        let dict = {
-            'rockrock': ['Tie!', -1],
-            'paperpaper': ['Tie!', -1],
-            'scissorsscissors': ['Tie!', -1],
-            'rockpaper': ['You Lose! Paper beats rock.', 0],
-            'rockscissors': ['You Win! Rock beats scissors.', 1],
-            'paperrock': ['You Win! Paper beats rock.', 1],
-            'paperscissors': ['You Lose! Scissors beats paper.', 0],
-            'scissorsrock': ['You Lose! Rock beats scissors.', 0],
-            'scissorspaper': ['You Win! Scissors beats paper.', 1],
-        };
-        
-        let combo = playerSelection + computerSelection;
-    
-        return dict[combo];
-    }
+    let combo = playerSelection + computerSelection;
+
+    switch(dict[combo][1]) {
+        case -1:
+            break;
+        case 0:
+            computerScore++
+            break;
+        case 1:
+            playerScore++
+            break;
+    };
+
+    let scorePrint = document.querySelector('.score-print')
+
+    if (playerScore == 5) {
+        scorePrint.textContent = dict[combo][0] + ` You won the game ${playerScore}-${computerScore}.`
+        resetGame();
+    } else if (computerScore == 5) {
+        scorePrint.textContent = dict[combo][0] + ` You lost the game ${playerScore}-${computerScore}.`
+        resetGame();
+    } else {
+        scorePrint.textContent = dict[combo][0] + ` The score is ${playerScore}-${computerScore}.`
+    } 
 }
 
-game();
+function resetGame(){
+    playerScore = 0;
+    computerScore = 0;
+}
+
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('.button-selection');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playRound(button.id, computerPlay())
+    });
+});
