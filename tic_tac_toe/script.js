0fb96f3189830c964fb58fcd67264dab8f7ee9b7
Tyler  Doblanko <tylerdoblanko@gmail.com>
2021-12-31 14:07:02 -0700

    added logic to check for winner

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -24,19 +24,21 @@ const Gameboard = (() => {
     [0, 0, 0],
     [0, 0, 0]]
 
-    const changeScore = (row, col, player) => {
-        gameArr[row][col] = player
-    }
-    
-    const resetScore = () => {
+    const _compareMatrix = (matrix1, matrix2) => {
+        // check if 2 matrices are equivalent
 
-    }
-
-    const getScore = () => {
-
+        if (matrix1[0] == matrix2[0]) {
+            if (matrix1[1] == matrix2[1]) {
+               if (matrix1[2] == matrix2[2]) {
+                    return true
+               }
+            }
+        }
     }
 
     const checkIfAvail = (row, col, symbol) => {
+        // check if position is available to make a play
+        // if it is available, make play
         if (gameArr[row][col] == 0) {
             gameArr[row][col] = symbol
             return true
@@ -45,11 +47,35 @@ const Gameboard = (() => {
         }
     }
 
-    const checkWinner = () => {
+    const checkWinner = (currentPlayerSymbol) => {
+        let winningArr = [currentPlayerSymbol, currentPlayerSymbol, currentPlayerSymbol]
+        // check rows then columns
+        for (let i = 0; i < 3; i++) {
+            
+            if (_compareMatrix(gameArr[i], winningArr)) {return true}
 
+            let column = []
+            for (let j = 0; j < 3; j ++) {
+                column.push(gameArr[j][i])
+            }
+
+            if (_compareMatrix(column, winningArr)) {return true}
+        }
+
+        // check diagonals
+        let diagonal1 = [gameArr[0][0], gameArr[1][1], gameArr[2][2]]
+        let diagonal2 = [gameArr[0][2], gameArr[1][1], gameArr[2][0]]
+
+        if (_compareMatrix(diagonal1, winningArr)) {return true}
+        if (_compareMatrix(diagonal2, winningArr)) {return true}
+
+
+        return false
     }
 
-    return {changeScore, getScore, resetScore, checkWinner, checkIfAvail}
+    
+
+    return {checkWinner, checkIfAvail}
 })()
 
 // Module to control the flow of the game
@@ -69,11 +95,21 @@ const gameController = (() => {
     }
 
     const makePlay = (boxID) => {
+        // make a play on the board
+
         // compute the location from the box ID
         row = Math.floor((boxID - 1) / 3)
         col = ((boxID - 1) % 3)
+
+        // Make the play if the box is available
+        // Render the play, check for winner, then change turns
         if (Gameboard.checkIfAvail(row, col, game.currentPlayer.getSymbol())) {
             displayController.render(boxID, game.currentPlayer.getSymbol())
+            
+            if (Gameboard.checkWinner(game.currentPlayer.getSymbol())) {
+                displayController.winner(game.currentPlayer)
+            }
+            
             changeTurns()
         }
     }
@@ -103,7 +139,11 @@ const displayController = (() => {
         document.querySelectorAll(".box").forEach(box => {box.textContent = ""})
     }
 
-    return { render, resetGrid }
+    const winner = (currentPlayer) => {
+        document.getElementById("winner").textContent = `${currentPlayer.getName()} wins!`
+    }
+
+    return { render, resetGrid, winner }
 })()
 
 // Event Listeners
