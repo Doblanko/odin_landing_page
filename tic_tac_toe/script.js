2736e1254c4b70dae5ebe0755f64c70ce249890a
Tyler  Doblanko <tylerdoblanko@gmail.com>
2021-12-30 20:52:08 -0700

    added ability to click boxes

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -1,7 +1,7 @@
-
 // Factory function for players
-const Player = (player) => {
+const Player = (player, symbol) => {
     let playerName = player
+    let playerSymbol = symbol
 
     const setName = name => {
         if (name != "") {
@@ -9,9 +9,12 @@ const Player = (player) => {
         }
     }
 
-    const getName = () => {return playerName}
+    const getName = () => playerName
 
-    return { getString, getSimpleString, setName, getName }
+    const getSymbol = () => playerSymbol
+
+
+    return { setName, getName, getSymbol  }
 }
 
 // Module for the GameBoard functionality
@@ -34,7 +37,11 @@ const Gameboard = (() => {
     }
 
     const checkIfAvail = (row, col) => {
-
+        if (gameArr[row][col] == 0) {
+            return true
+        } else {
+            return false
+        }
     }
 
     const checkWinner = () => {
@@ -46,12 +53,72 @@ const Gameboard = (() => {
 
 // Module to control the flow of the game
 const gameController = (() => {
+    let game = {
+        "player1": null,
+        "player2": null,
+        "currentPlayer": 0,
+        "isTied": false,
+        "isOver": false,
+    }
 
-})
+    const startGame = (player1, player2) => {
+        game.player1 = player1
+        game.player2 = player2
+        game.currentPlayer = game.player1
+        console.log(game.player1.getSymbol())
+        console.log(game.player2.getSymbol())
+    }
+
+    const makePlay = (boxID) => {
+        // compute the location from the box ID
+        row = Math.floor((boxID - 1) / 3)
+        col = ((boxID - 1) % 3)
+        if (Gameboard.checkIfAvail(row, col)) {
+            displayController.render(boxID, game.currentPlayer.getSymbol())
+            changeTurns()
+        }
+    }
+
+    const changeTurns = () => {
+        if (game.currentPlayer == game.player1) {
+            game.currentPlayer = game.player2
+        } else {
+            game.currentPlayer = game.player1
+        }
+        
+    }
+
+    return { startGame, makePlay }
+})()
 
 // Module for display control
 const displayController = (() => {
-    let status = {
-        currentPlayer: '',
+  
+    const render = (boxID, symbol) => {
+        let box = document.getElementById(`${boxID}`)
+        box.textContent = symbol
     }
+
+    const resetGrid = () => {
+        // Removes all entries in the board
+        document.querySelectorAll(".box").forEach(box => {box.textContent = ""})
+    }
+
+    return { render, resetGrid }
 })()
+
+// Event Listeners
+const gridBoxes = document.querySelectorAll(".box")
+gridBoxes.forEach((box) => {
+    box.addEventListener('click', boxClick)
+})
+
+// Listener function
+function boxClick(event) {
+    // make the play
+    gameController.makePlay(event.target.id)
+}
+
+let player1 = Player("Player 1", "X")
+let player2 = Player("Player 2", "O")
+gameController.startGame(player1, player2)
